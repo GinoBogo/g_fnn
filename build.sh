27f3a3bbc380f2c86dc5cea64fc44dcebdbc2d48
Gino Bogo <gino.bogo@gmail.com>
2025-03-26 16:08:56 +0100

    Update of build.sh script.

--- build.sh
+++ build.sh
@@ -5,14 +5,20 @@ set -euo pipefail
 
 clear
 
+BUILD_TYPE=${1:-Debug}
+
+echo "Build type: $BUILD_TYPE"
+
 if [ ! -d "build" ]; then
     mkdir build
 fi
 
 cd ./build
 
-cmake ".."
+cmake "../CmakeLists.txt" -B ./ -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
-make
+cmake --build ./ --config $BUILD_TYPE
+
+cp ./compile_commands.json ../
 
 echo '... Done!'
