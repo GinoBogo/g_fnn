6790c3b10e7c7563370b4d639e3afb860370cd3a
Gino Bogo <gino.bogo@gmail.com>
2025-04-05 09:46:15 +0200

    README - update of Box-Muller transform code.

--- resources/examples/code_04.c
+++ resources/examples/code_04.c
@@ -7,17 +7,20 @@ static float rand_normal(void) {
     static int   n2_cached = 0;
 
     if (!n2_cached) {
-        float x, y, r;
+        float x, y, r, n1;
         do {
-            x = 2.0f * rand() / (float)RAND_MAX - 1;
-            y = 2.0f * rand() / (float)RAND_MAX - 1;
+            x = 2.0f * rand() / (float)RAND_MAX - 1.0f;
+            y = 2.0f * rand() / (float)RAND_MAX - 1.0f;
             r = x * x + y * y;
         } while (r == 0.0f || r >= 1.0f);
 
-        float d   = sqrtf(-2.0f * logf(r) / r);
-        n2        = y * d;
+        float d = sqrtf(-2.0f * logf(r) / r);
+
+        n1 = x * d;
+        n2 = y * d;
+
         n2_cached = 1;
-        return x * d;
+        return n1;
     } else {
         n2_cached = 0;
         return n2;
