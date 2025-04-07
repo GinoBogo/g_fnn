0005cc679a0aade053bea6187d6cf9090e96776c
Gino Bogo <gino.bogo@gmail.com>
2025-04-07 13:56:10 +0200

    "data_reader" - added the "data_reader_next_matrix" function.

--- examples/data_reader.h
+++ examples/data_reader.h
@@ -12,11 +12,15 @@
 #include <stdbool.h> // bool
 #include <stdio.h>   // FILE
 
+#include "g_page.h" // f_matrix_t
+
 FILE *data_reader_open(const char *filename);
 
 void data_reader_close(FILE **file);
 
 bool data_reader_next_values(FILE *file, float *values_ptr, const int values_len);
+
+bool data_reader_next_matrix(FILE *file, f_matrix_t *matrix_ptr);
 
 #endif // DATA_READER_H
 
