0fabd24194f9a29f1eccc04e9e207f726ad88a64
Gino Bogo <gino.bogo@gmail.com>
2025-04-09 12:57:12 +0200

    "g_fnn_7segment_led" - update of "main" code.

--- examples/g_fnn_7segment_led/main.c
+++ examples/g_fnn_7segment_led/main.c
@@ -17,18 +17,143 @@
 
 #define TRAINING_MODE 1
 
+// -------------------------------------------------------------------------
+// network topology definition
+// -------------------------------------------------------------------------
+
+// layer 0: virtual input layer (store outputs only)
+float L00_Y[7] = {0.0f};
+
+// layer 1: hidden layer
+float             L01_W[20][SIZEOF(L00_Y) + 1] = {{0.0f}};
+float             L01_Z[SIZEOF(L01_W)]         = {0.0f};
+float             L01_Y[SIZEOF(L01_Z)]         = {0.0f};
+float             L01_dY_dZ[SIZEOF(L01_Y)]     = {0.0f};
+float             L01_dE_dY[SIZEOF(L01_Y)]     = {0.0f};
+float             L01_LR                       = 0.01f;
+g_act_func_type_t L01_AF_TYPE                  = LEAKY_RELU;
+float             L01_AF_ARGS[1]               = {0.01f};
+
+// layer 2: hidden layer
+float             L02_W[20][SIZEOF(L01_Y) + 1] = {{0.0f}};
+float             L02_Z[SIZEOF(L02_W)]         = {0.0f};
+float             L02_Y[SIZEOF(L02_Z)]         = {0.0f};
+float             L02_dY_dZ[SIZEOF(L02_Y)]     = {0.0f};
+float             L02_dE_dY[SIZEOF(L02_Y)]     = {0.0f};
+float             L02_LR                       = 0.02f;
+g_act_func_type_t L02_AF_TYPE                  = LEAKY_RELU;
+float             L02_AF_ARGS[1]               = {0.01f};
+
+// layer 3: output layer
+float             L03_W[10][SIZEOF(L02_Y) + 1] = {{0.0f}};
+float             L03_Z[SIZEOF(L03_W)]         = {0.0f};
+float             L03_Y[SIZEOF(L03_Z)]         = {0.0f};
+float             L03_dY_dZ[SIZEOF(L03_Y)]     = {0.0f};
+float             L03_dE_dY[SIZEOF(L03_Y)]     = {0.0f};
+float             L03_LR                       = 0.03f;
+g_act_func_type_t L03_AF_TYPE                  = SIGMOID;
+float             L03_AF_ARGS[1]               = {0.0f};
+
+// layer 3: actual outputs (Y target)
+float L03_YT[SIZEOF(L03_Y)] = {0.0f};
+
+// -----------------------------------------------------------------------------
+// File Handles
 // -----------------------------------------------------------------------------
 
-FILE *network_weights_file = NULL;
-FILE *network_dataset_file = NULL;
-FILE *actual_outputs_file  = NULL;
-FILE *network_outputs_file = NULL;
+FILE *file_weights_cfg = NULL;
+FILE *file_dataset_set = NULL;
+FILE *file_outputs_act = NULL;
+FILE *file_weights_out = NULL;
+FILE *file_outputs_out = NULL;
 
 static void cleanup_resources(void) {
-    data_reader_close(&network_weights_file);
-    data_reader_close(&network_dataset_file);
-    data_reader_close(&actual_outputs_file);
-    data_writer_close(&network_outputs_file);
+    data_reader_close(&file_weights_cfg);
+    data_reader_close(&file_dataset_set);
+    data_reader_close(&file_outputs_act);
+    data_writer_close(&file_weights_out);
+    data_writer_close(&file_outputs_out);
+}
+
+// -----------------------------------------------------------------------------
+// Network Mode: TRAINING
+// -----------------------------------------------------------------------------
+
+void training_mode(g_network_t *network, g_pages_t *pages) {
+    // layer 3: actual outputs
+    f_vector_t actual_outputs;
+    actual_outputs.ptr = &L03_YT[0];
+    actual_outputs.len = SIZEOF(L03_YT);
+
+    network->Init_Weights(network, 0.5f);
+
+    file_outputs_act = data_reader_open("fnn_outputs.act");
+    if (file_outputs_act == NULL) {
+        network->Destroy(network);
+        exit(1);
+    }
+
+    file_weights_out = data_writer_open("fnn_weights.out");
+    if (file_weights_out == NULL) {
+        network->Destroy(network);
+        exit(1);
+    }
+
+    // load dataset from file
+    while (data_reader_next_vector(file_dataset_set, &pages->ptr[0].x)) {
+        network->Step_Forward(network);
+
+        // load actual outputs from file
+        if (data_reader_next_vector(file_outputs_act, &actual_outputs)) {
+            network->Step_Errors(network, &actual_outputs);
+
+            network->Step_Backward(network);
+        }
+
+        // save outputs to file
+        const int L = pages->len - 1;
+        if (!data_writer_next_vector(file_outputs_out, &pages->ptr[L].y)) {
+            exit(1);
+        }
+    }
+
+    // save weights to file
+    data_writer_next_matrix(file_weights_out, &pages->ptr[0].w);
+    data_writer_next_matrix(file_weights_out, &pages->ptr[1].w);
+    data_writer_next_matrix(file_weights_out, &pages->ptr[2].w);
+}
+
+// -----------------------------------------------------------------------------
+// Network Mode: INFERENCE
+// -----------------------------------------------------------------------------
+
+void inference_mode(g_network_t *network, g_pages_t *pages) {
+    // load weights from file
+    file_weights_cfg = data_reader_open("fnn_weights.cfg");
+    if (file_weights_cfg == NULL) {
+        network->Destroy(network);
+        exit(1);
+    }
+
+    for (int k = 0; k < pages->len; ++k) {
+        if (!data_reader_next_matrix(file_weights_cfg, &pages->ptr[k].w)) {
+            network->Destroy(network);
+            exit(1);
+        }
+    }
+    data_reader_close(&file_weights_cfg);
+
+    // load dataset from file
+    while (data_reader_next_vector(file_dataset_set, &pages->ptr[0].x)) {
+        network->Step_Forward(network);
+
+        // save outputs to file
+        const int L = pages->len - 1;
+        if (!data_writer_next_vector(file_outputs_out, &pages->ptr[L].y)) {
+            network->Destroy(network);
+            exit(1);
+        }
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -43,50 +168,9 @@ int main(int argc, char *argv[]) {
     atexit(cleanup_resources);
 
     // -------------------------------------------------------------------------
-    // network topology definition
-    // -------------------------------------------------------------------------
-
-    // layer 0: virtual input layer (store outputs only)
-    float L00_Y[7] = {0.0f};
-
-    // layer 1: hidden layer
-    float             L01_W[20][SIZEOF(L00_Y) + 1] = {{0.0f}};
-    float             L01_Z[SIZEOF(L01_W)]         = {0.0f};
-    float             L01_Y[SIZEOF(L01_Z)]         = {0.0f};
-    float             L01_dY_dZ[SIZEOF(L01_Y)]     = {0.0f};
-    float             L01_dE_dY[SIZEOF(L01_Y)]     = {0.0f};
-    float             L01_LR                       = 0.01f;
-    g_act_func_type_t L01_AF_TYPE                  = LEAKY_RELU;
-    float             L01_AF_ARGS[1]               = {0.01f};
-
-    // layer 2: hidden layer
-    float             L02_W[20][SIZEOF(L01_Y) + 1] = {{0.0f}};
-    float             L02_Z[SIZEOF(L02_W)]         = {0.0f};
-    float             L02_Y[SIZEOF(L02_Z)]         = {0.0f};
-    float             L02_dY_dZ[SIZEOF(L02_Y)]     = {0.0f};
-    float             L02_dE_dY[SIZEOF(L02_Y)]     = {0.0f};
-    float             L02_LR                       = 0.02f;
-    g_act_func_type_t L02_AF_TYPE                  = LEAKY_RELU;
-    float             L02_AF_ARGS[1]               = {0.01f};
-
-    // layer 3: output layer
-    float             L03_W[10][SIZEOF(L02_Y) + 1] = {{0.0f}};
-    float             L03_Z[SIZEOF(L03_W)]         = {0.0f};
-    float             L03_Y[SIZEOF(L03_Z)]         = {0.0f};
-    float             L03_dY_dZ[SIZEOF(L03_Y)]     = {0.0f};
-    float             L03_dE_dY[SIZEOF(L03_Y)]     = {0.0f};
-    float             L03_LR                       = 0.03f;
-    g_act_func_type_t L03_AF_TYPE                  = SIGMOID;
-    float             L03_AF_ARGS[1]               = {0.0f};
-
-#if TRAINING_MODE
-    // layer 3: actual outputs (Y target)
-    float L03_YT[SIZEOF(L03_Y)] = {0.0f};
-#endif
-
-    // -------------------------------------------------------------------------
     // page structure
     // -------------------------------------------------------------------------
+
     g_page_t page[3];
 
     for (int k = 0; k < SIZEOF(page); ++k) {
@@ -151,27 +235,6 @@ int main(int argc, char *argv[]) {
     page[2].af_args.ptr = L03_AF_ARGS;
     page[2].af_args.len = SIZEOF(L03_AF_ARGS);
 
-#if TRAINING_MODE
-    // layer 3: actual outputs
-    f_vector_t actual_outputs;
-    actual_outputs.ptr = &L03_YT[0];
-    actual_outputs.len = SIZEOF(L03_YT);
-#else
-    // load weights from file
-    network_weights_file = data_reader_open("network_weights.txt");
-    if (network_weights_file == NULL) {
-        return 1;
-    }
-
-    for (int k = 0; k < SIZEOF(page); ++k) {
-        if (!data_reader_next_matrix(network_weights_file, &page[k].w)) {
-            data_reader_close(&network_weights_file);
-            return 1;
-        }
-    }
-    data_reader_close(&network_weights_file);
-#endif
-
     // -------------------------------------------------------------------------
     // pages structure
     // -------------------------------------------------------------------------
@@ -188,53 +251,29 @@ int main(int argc, char *argv[]) {
     g_network_link(&network);
 
     if (network.Create(&network, &pages)) {
-#if TRAINING_MODE
-        network.Init_Weights(&network, 0.5f);
-#endif
-        // network dataset stream
-        network_dataset_file = data_reader_open("network_dataset.txt");
-        if (network_dataset_file == NULL) {
-            network.Destroy(&network);
-            return 1;
-        }
-#if TRAINING_MODE
-        // actual outputs stream
-        actual_outputs_file = data_reader_open("actual_outputs.txt");
-        if (actual_outputs_file == NULL) {
-            data_reader_close(&network_dataset_file);
-            network.Destroy(&network);
-            return 1;
-        }
-#endif
-        // network outputs stream
-        network_outputs_file = data_writer_open("network_outputs.txt");
-        if (network_outputs_file == NULL) {
-            data_reader_close(&network_dataset_file);
-            data_reader_close(&actual_outputs_file);
+        // load dataset from file
+        file_dataset_set = data_reader_open("fnn_dataset.set");
+        if (file_dataset_set == NULL) {
             network.Destroy(&network);
             return 1;
         }
 
-        while (data_reader_next_values(network_dataset_file, L00_Y, SIZEOF(L00_Y))) {
-            network.Step_Forward(&network);
-#if TRAINING_MODE
-            if (data_reader_next_values(actual_outputs_file, L03_YT, SIZEOF(L03_YT))) {
-                network.Step_Errors(&network, &actual_outputs);
-
-                network.Step_Backward(&network);
-            }
-#endif
-            if (!data_writer_next_values(network_outputs_file, L03_Y, SIZEOF(L03_Y))) {
-                break;
-            }
+        // save outputs to file
+        file_outputs_out = data_writer_open("fnn_outputs.out");
+        if (file_outputs_out == NULL) {
+            network.Destroy(&network);
+            return 1;
         }
 
-        data_reader_close(&network_dataset_file);
-        data_reader_close(&actual_outputs_file);
-        data_writer_close(&network_outputs_file);
+#if TRAINING_MODE
+        training_mode(&network, &pages);
+#else
+        inference_mode(&network, &pages);
+#endif
+        network.Destroy(&network);
     }
 
-    network.Destroy(&network);
+    cleanup_resources();
 
     puts("... Done!");
     return 0;
